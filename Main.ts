4215ff92d725399d18b562cb37acccd73a8f498e
Artiom <artemilico1337@gmail.com>
2025-02-16 16:21:47 +0200

    Final solution

---
+++ Main.ts
@@ -0,0 +1,71 @@
+import { Grammar } from './Grammar';
+import * as readline from 'readline';
+
+function generateRandomString(vT: Set<string>): string {
+    let someString = '';
+    const alphabetArray = Array.from(vT);
+    const length = Math.floor(Math.random() * (10 - 3 + 1)) + 3;
+
+    for (let i = 0; i < length; i++) {
+        someString += alphabetArray[Math.floor(Math.random() * alphabetArray.length)];
+    }
+    return someString;
+}
+
+console.log("Laboratory Work 1: Intro to formal languages. Regular grammars. Finite Automata.\n");
+console.log("Student: Ilico Artemie\nGroup: FAF-231\nVariant 17\n");
+
+const vN = new Set(["S", "A", "B", "C"]);
+const vT = new Set(['a', 'b', 'c', 'd']);
+const p = new Map<string, string[]>([
+    ["S", ["dA"]],
+    ["A", ["aB", "bA"]],
+    ["B", ["bC", "aB", "d"]],
+    ["C", ["cB"]]
+]);
+const s = "S";
+
+const grammar = new Grammar(vN, vT, p, s);
+console.log("Grammar:");
+console.log(grammar.toString());
+
+console.log("\n5 valid strings by grammar:");
+for (let i = 0; i < 5; i++) {
+    console.log(`\nString ${i + 1}:`);
+    const generatedString = grammar.createWord();
+    console.log("Final string:", generatedString);
+}
+
+const finiteAutomaton = grammar.toFiniteAutomaton();
+console.log("\nConverted Finite Automaton:");
+console.log(finiteAutomaton.toString());
+
+console.log("\nTesting random strings with the Finite Automaton:");
+for (let i = 0; i < 5; i++) {
+    const testString = generateRandomString(vT);
+    console.log(`\nTest string ${i + 1}: ${testString}`);
+    const belongs = finiteAutomaton.stringBelongToLanguage(testString);
+    console.log(`String belongs to language: ${belongs}`);
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log("\nEnter a string to check if it belongs to the language (type 'exit' to quit):");
+
+function promptInput() {
+    rl.question("> ", (inputString) => {
+        if (inputString.toLowerCase() === "exit") {
+            console.log("Exiting...");
+            rl.close();
+        } else {
+            const belongs = finiteAutomaton.stringBelongToLanguage(inputString);
+            console.log(`String belongs to language: ${belongs}`);
+            promptInput();
+        }
+    });
+}
+
+promptInput();
